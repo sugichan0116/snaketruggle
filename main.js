56858ce3d22fae61fbfe9224777b3924676fb030
sugichan0116 <32512315+sugichan0116@users.noreply.github.com>
2018-05-22 13:53:13 +0900

    test

--- main.js
+++ main.js
@@ -40,8 +40,17 @@ window.onload = function() {
       res.se.clear,
       res.se.move
     ];
+    // Pass true to enable retina display, disabled by default to improve performance
+    cc.view.enableRetina(false);
+    // Adjust viewport meta
+    cc.view.adjustViewPort(true);
+    // Setup the resolution policy and design resolution size
+    cc.view.setDesignResolutionSize(800, 450, cc.ResolutionPolicy.SHOW_ALL);
+    // The game will be resized when browser size change
+    cc.view.resizeWithBrowserSize(true);
+
     cc.LoaderScene.preload(preload_res, function() {
-      //var MyScene = new TitleScene();
+
       console.log(TitleScene);
       cc.director.runScene(new TitleScene());
     }, this);
